df52b43664925ede2f120b8da372d31587b9bfcb
Michael Vieck <vieck@purdue.edu>
2018-09-09 19:11:17 -0400

    Create Thread added.

---
+++ Multithreading and Async Processing/CreateThread/Multithreading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Multithreading
+{
+	//Creating a thread with the thread class
+    class MainClass
+    {
+		public static void CreateThread() {
+			Thread thread = new Thread(new ThreadStart(SleepThread));
+			thread.Start();
+		}
+
+		private static void SleepThread() {
+			for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Sleeping Thread: " + i);
+                Thread.Sleep(0);
+            }
+		}
+
+        public static void Main(string[] args)
+        {
+			CreateThread();
+        }
+    }
+}
